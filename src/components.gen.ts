731733b4026e4a8f0d96547968278e59c708472c
Tomas Santana <tomas.30604530@uru.edu>
2025-10-12 12:06:35 -0400

    delete demo2 component

--- src/components.gen.ts
+++ src/components.gen.ts
@@ -1,8 +1,11 @@
 // AUTO-GENERATED FILE. Do not edit manually.
 // Updated by src/scripts/createComponent.js
 import type { DemoProps } from './Components/Visual/Demo/Demo.types';
+import type Demo from './Components/Visual/Demo/Demo';
+import type Demo2 from './Components/Visual/Demo2/Demo2';
+import type HomePage from './Components/AppComponents/HomePage/HomePage';
 
-export type ComponentName = 'Button' | 'Card' | 'Checkbox' | 'CodeVisualizer' | 'Demo' | 'Details' | 'DropDown' | 'Grid' | 'Icon' | 'Input' | 'Layout' | 'Loading' | 'MultiRoute' | 'Navbar' | 'NotFound' | 'Route' | 'Select' | 'Switch' | 'TreeItem' | 'TreeView' | 'FetchManager' | 'IndexedDbManager' | 'Link' | 'LocalStorageManager' | 'Translator' | 'HomePage' | 'Playground';
+export type ComponentName = 'Button' | 'Card' | 'Checkbox' | 'CodeVisualizer' | 'Demo' | 'Demo2' | 'Details' | 'DropDown' | 'Grid' | 'Icon' | 'Input' | 'Layout' | 'Loading' | 'MultiRoute' | 'Navbar' | 'NotFound' | 'Route' | 'Select' | 'Switch' | 'TreeItem' | 'TreeView' | 'FetchManager' | 'IndexedDbManager' | 'Link' | 'LocalStorageManager' | 'Translator' | 'HomePage' | 'Playground';
 
 export type ComponentPropsMap = {
   'Button': any,
@@ -31,5 +34,35 @@ export type ComponentPropsMap = {
   'LocalStorageManager': any,
   'Translator': any,
   'HomePage': any,
+  'Playground': any
+};
+
+export type ComponentInstanceMap = {
+  'Button': any,
+  'Card': any,
+  'Checkbox': any,
+  'CodeVisualizer': any,
+  'Demo': Demo,
+  'Details': any,
+  'DropDown': any,
+  'Grid': any,
+  'Icon': any,
+  'Input': any,
+  'Layout': any,
+  'Loading': any,
+  'MultiRoute': any,
+  'Navbar': any,
+  'NotFound': any,
+  'Route': any,
+  'Select': any,
+  'Switch': any,
+  'TreeItem': any,
+  'TreeView': any,
+  'FetchManager': any,
+  'IndexedDbManager': any,
+  'Link': any,
+  'LocalStorageManager': any,
+  'Translator': any,
+  'HomePage': HomePage,
   'Playground': any
 };
