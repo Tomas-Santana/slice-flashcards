35499089af2c3cb855c4fc8a13b38b470eaf05f3
Tomas Santana <tomas.30604530@uru.edu>
2025-10-16 05:15:39 -0400

    chore: add tailwind.config.js for Tailwind CSS configuration

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/App/style.css",
+    "./src/**/Components/**/*.{js,ts,html}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
