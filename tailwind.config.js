28e74366b0df512e80e07b3be33a3eda1bafb9a9
Tomas Santana <tomas.30604530@uru.edu>
2025-10-21 20:10:46 -0400

    feat: enhance Tailwind CSS configuration with additional color definitions

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/App/style.css",
-    "./src/**/Components/**/*.{js,ts,html}",
-  ],
+  content: ["./src/**/App/style.css", "./src/**/Components/**/*.{js,ts,html}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "var(--primary-color)",
+        "primary-rgb": "var(--primary-color-rgb)",
+        "primary-contrast": "var(--primary-color-contrast)",
+        "primary-bg": "var(--primary-background-color)",
+        "primary-shade": "var(--primary-color-shade)",
+
+        secondary: "var(--secondary-color)",
+        "secondary-rgb": "var(--secondary-color-rgb)",
+        "secondary-bg": "var(--secondary-background-color)",
+        "secondary-contrast": "var(--secondary-color-contrast)",
+
+        tertiary: "var(--tertiary-background-color)",
+
+        success: "var(--success-color)",
+        "success-contrast": "var(--success-contrast)",
+
+        warning: "var(--warning-color)",
+        "warning-contrast": "var(--warning-contrast)",
+
+        danger: "var(--danger-color)",
+        "danger-contrast": "var(--danger-contrast)",
+
+        medium: "var(--medium-color)",
+        "medium-contrast": "var(--medium-contrast)",
+
+        disabled: "var(--disabled-color)",
+      },
+    },
   },
   plugins: [],
-}
-
+};
