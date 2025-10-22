39c20716ddd2bce767f69e7d745f7adab343c5aa
Tomas Santana <tomas.30604530@uru.edu>
2025-10-21 20:09:59 -0400

    feat: add route guards

--- src/App/index.js
+++ src/App/index.js
@@ -1,63 +1,86 @@
-import Slice from '/Slice/Slice.js';
+import Slice from "/Slice/Slice.js";
+import { openDatabase } from "../Components/Service/DB/openDatabase";
 
-const CACHE_NAME = 'slice-cache-v1';
+const CACHE_NAME = "slice-cache-v1";
 const urlsToCache = [
-   // Añade aquí las rutas de los archivos que deseas almacenar
-   './Slice/Slice.js',
-   './Slice/Components/Structural/Logger/Logger.js',
-   './Slice/Components/Structural/Debugger/Debugger.js',
-   './Slice/Components/Structural/Router/Router.js',
-   './Slice/Components/Structural/Router/routes.js',
-   './Slice/Components/Structural/Controller/Controller.js',
-   './Slice/Components/Structural/StylesManager/StylesManager.js',
-   './Slice/Components/Structural/StylesManager/ThemeManager/ThemeManager.js',
+  // Añade aquí las rutas de los archivos que deseas almacenar
+  "./Slice/Slice.js",
+  "./Slice/Components/Structural/Logger/Logger.js",
+  "./Slice/Components/Structural/Debugger/Debugger.js",
+  "./Slice/Components/Structural/Router/Router.js",
+  "./Slice/Components/Structural/Router/routes.js",
+  "./Slice/Components/Structural/Controller/Controller.js",
+  "./Slice/Components/Structural/StylesManager/StylesManager.js",
+  "./Slice/Components/Structural/StylesManager/ThemeManager/ThemeManager.js",
 ];
 
-self.addEventListener('install', (event) => {
-   event.waitUntil(
-      caches.open(CACHE_NAME).then((cache) => {
-         console.log('Opened cache');
-         return cache.addAll(urlsToCache);
-      })
-   );
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
-self.addEventListener('fetch', (event) => {
-   event.respondWith(
-      caches.match(event.request).then((response) => {
-         if (response) {
-            return response;
-         }
-         return fetch(event.request);
-      })
-   );
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
 });
 
-self.addEventListener('activate', (event) => {
-   const cacheWhitelist = [CACHE_NAME];
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
 
-   event.waitUntil(
-      caches.keys().then((cacheNames) => {
-         return Promise.all(
-            cacheNames.map((cacheName) => {
-               if (cacheWhitelist.indexOf(cacheName) === -1) {
-                  return caches.delete(cacheName);
-               }
-            })
-         );
-      })
-   );
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
 
-if ('serviceWorker' in navigator) {
-   window.addEventListener('load', () => {
-      navigator.serviceWorker
-         .register('/service-worker.js')
-         .then((registration) => {
-            console.log('Service Worker registrado con éxito:', registration);
-         })
-         .catch((error) => {
-            console.log('Error al registrar el Service Worker:', error);
-         });
-   });
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then((registration) => {
+        console.log("Service Worker registrado con éxito:", registration);
+      })
+      .catch((error) => {
+        console.log("Error al registrar el Service Worker:", error);
+      });
+  });
 }
+
+window.slice.router.beforeEach(async (to, from, next) => {
+  const db = openDatabase();
+  const langSetting = await db.get("settings", "settings");
+
+  if (to.metadata?.requiresNoSettings) {
+    if (langSetting !== undefined) {
+      console.log("Settings found, redirecting to home");
+      return next({ path: "/" });
+    }
+    return true;
+  } else {
+    if (langSetting === undefined) {
+      console.log("No settings found, redirecting to start");
+      return next({ path: "/start" });
+    }
+  }
+
+  return next();
+});
+
+window.slice.router.start();
