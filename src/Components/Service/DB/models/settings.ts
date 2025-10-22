141be6dcfd47846f0e15c3e3d4696eab6abaf39c
Tomas Santana <tomas.30604530@uru.edu>
2025-10-21 20:13:50 -0400

    feat: add name property to Settings interface

--- src/Components/Service/DB/models/settings.ts
+++ src/Components/Service/DB/models/settings.ts
@@ -2,6 +2,7 @@ import type { LanguageCode } from '@/lib/types/languages';
 
 export interface Settings {
   id: 'settings'; 
+  name: string;
   baseLanguage: LanguageCode;
   selectedLanguage: LanguageCode;
   languages: LanguageCode[];
