866d95f92cff3c75edbeb08eac5a53fba8166a6c
Tomas Santana <tomas.30604530@uru.edu>
2025-10-21 20:12:36 -0400

    feat: update language names to native scripts in languages.ts

--- src/lib/types/languages.ts
+++ src/lib/types/languages.ts
@@ -1,212 +1,206 @@
 const languages = [
-  { code: "ab", name: "Abkhazian" },
-  { code: "aa", name: "Afar" },
-  { code: "af", name: "Afrikaans" },
-  { code: "ak", name: "Akan" },
-  { code: "sq", name: "Albanian" },
-  { code: "am", name: "Amharic" },
-  { code: "ar", name: "Arabic" },
-  { code: "an", name: "Aragonese" },
-  { code: "hy", name: "Armenian" },
-  { code: "as", name: "Assamese" },
-  { code: "av", name: "Avaric" },
-  { code: "ae", name: "Avestan" },
-  { code: "ay", name: "Aymara" },
-  { code: "az", name: "Azerbaijani" },
-  { code: "bm", name: "Bambara" },
-  { code: "ba", name: "Bashkir" },
-  { code: "eu", name: "Basque" },
-  { code: "be", name: "Belarusian" },
-  { code: "bn", name: "Bengali" },
-  { code: "bh", name: "Bihari languages" },
-  { code: "bi", name: "Bislama" },
-  { code: "bs", name: "Bosnian" },
-  { code: "br", name: "Breton" },
-  { code: "bg", name: "Bulgarian" },
-  { code: "my", name: "Burmese" },
-  { code: "ca", name: "Catalan, Valencian" },
-  { code: "km", name: "Central Khmer" },
-  { code: "ch", name: "Chamorro" },
-  { code: "ce", name: "Chechen" },
-  { code: "ny", name: "Chichewa, Chewa, Nyanja" },
-  { code: "zh", name: "Chinese" },
-  { code: "cu", name: "Church Slavonic, Old Bulgarian, Old Church Slavonic" },
-  { code: "cv", name: "Chuvash" },
-  { code: "kw", name: "Cornish" },
-  { code: "co", name: "Corsican" },
-  { code: "cr", name: "Cree" },
-  { code: "hr", name: "Croatian" },
-  { code: "cs", name: "Czech" },
-  { code: "da", name: "Danish" },
-  { code: "dv", name: "Divehi, Dhivehi, Maldivian" },
-  { code: "nl", name: "Dutch, Flemish" },
-  { code: "dz", name: "Dzongkha" },
-  { code: "en", name: "English" },
-  { code: "eo", name: "Esperanto" },
-  { code: "et", name: "Estonian" },
-  { code: "ee", name: "Ewe" },
-  { code: "fo", name: "Faroese" },
-  { code: "fj", name: "Fijian" },
-  { code: "fi", name: "Finnish" },
-  { code: "fr", name: "French" },
-  { code: "ff", name: "Fulah" },
-  { code: "gd", name: "Gaelic, Scottish Gaelic" },
-  { code: "gl", name: "Galician" },
-  { code: "lg", name: "Ganda" },
-  { code: "ka", name: "Georgian" },
-  { code: "de", name: "German" },
-  { code: "ki", name: "Gikuyu, Kikuyu" },
-  { code: "el", name: "Greek (Modern)" },
-  { code: "kl", name: "Greenlandic, Kalaallisut" },
-  { code: "gn", name: "Guarani" },
-  { code: "gu", name: "Gujarati" },
-  { code: "ht", name: "Haitian, Haitian Creole" },
-  { code: "ha", name: "Hausa" },
-  { code: "he", name: "Hebrew" },
-  { code: "hz", name: "Herero" },
-  { code: "hi", name: "Hindi" },
-  { code: "ho", name: "Hiri Motu" },
-  { code: "hu", name: "Hungarian" },
-  { code: "is", name: "Icelandic" },
-  { code: "io", name: "Ido" },
-  { code: "ig", name: "Igbo" },
-  { code: "id", name: "Indonesian" },
+  { code: "ab", name: "Аҧсшәа" }, // Abkhazian
+  { code: "aa", name: "Qafár af" }, // Afar
+  { code: "af", name: "Afrikaans" }, // Afrikaans
+  { code: "ak", name: "Akan" }, // Akan
+  { code: "sq", name: "Shqip" }, // Albanian
+  { code: "am", name: "አማርኛ" }, // Amharic
+  { code: "ar", name: "العربية" }, // Arabic
+  { code: "an", name: "Aragonés" }, // Aragonese
+  { code: "hy", name: "Հայերեն" }, // Armenian
+  { code: "as", name: "অসমীয়া" }, // Assamese
+  { code: "av", name: "Авар мацӀ" }, // Avaric
+  { code: "ae", name: "Avesta" }, // Avestan
+  { code: "ay", name: "Aymar aru" }, // Aymara
+  { code: "az", name: "Azərbaycan dili" }, // Azerbaijani
+  { code: "bm", name: "Bamanankan" }, // Bambara
+  { code: "ba", name: "Башҡорт теле" }, // Bashkir
+  { code: "eu", name: "Euskara" }, // Basque
+  { code: "be", name: "Беларуская" }, // Belarusian
+  { code: "bn", name: "বাংলা" }, // Bengali
+  { code: "bh", name: "भोजपुरी" }, // Bihari languages (Usando el de Bhojpuri como ejemplo común)
+  { code: "bi", name: "Bislama" }, // Bislama
+  { code: "bs", name: "Bosanski" }, // Bosnian
+  { code: "br", name: "Brezhoneg" }, // Breton
+  { code: "bg", name: "Български" }, // Bulgarian
+  { code: "my", name: "မြန်မာစာ" }, // Burmese
+  { code: "ca", name: "Català" }, // Catalan, Valencian
+  { code: "km", name: "ភាសាខ្មែរ" }, // Central Khmer
+  { code: "ch", name: "Chamoru" }, // Chamorro
+  { code: "ce", name: "Нохчийн мотт" }, // Chechen
+  { code: "ny", name: "Chichewa" }, // Chichewa, Chewa, Nyanja
+  { code: "zh", name: "中文" }, // Chinese
+  { code: "cu", name: "Словѣ́ньскъ ѩзы́къ" }, // Church Slavonic, Old Bulgarian, Old Church Slavonic
+  { code: "cv", name: "Чăвашла" }, // Chuvash
+  { code: "kw", name: "Kernewek" }, // Cornish
+  { code: "co", name: "Corsu" }, // Corsican
+  { code: "cr", name: "Nēhiyawēwin" }, // Cree
+  { code: "hr", name: "Hrvatski" }, // Croatian
+  { code: "cs", name: "Čeština" }, // Czech
+  { code: "da", name: "Dansk" }, // Danish
+  { code: "dv", name: "ދިވެހި" }, // Divehi, Dhivehi, Maldivian
+  { code: "nl", name: "Nederlands" }, // Dutch, Flemish
+  { code: "dz", name: "རྫོང་ཁ" }, // Dzongkha
+  { code: "en", name: "English" }, // English (Se mantuvo "English" para la referencia)
+  { code: "eo", name: "Esperanto" }, // Esperanto
+  { code: "et", name: "Eesti" }, // Estonian
+  { code: "ee", name: "Eʋegbe" }, // Ewe
+  { code: "fo", name: "Føroyskt" }, // Faroese
+  { code: "fj", name: "Vosa Vakaviti" }, // Fijian
+  { code: "fi", name: "Suomi" }, // Finnish
+  { code: "fr", name: "Français" }, // French
+  { code: "ff", name: "Fulfulde" }, // Fulah
+  { code: "gd", name: "Gàidhlig" }, // Gaelic, Scottish Gaelic
+  { code: "gl", name: "Galego" }, // Galician
+  { code: "lg", name: "Luganda" }, // Ganda
+  { code: "ka", name: "ქართული" }, // Georgian
+  { code: "de", name: "Deutsch" }, // German
+  { code: "ki", name: "Gikuyu" }, // Gikuyu, Kikuyu
+  { code: "el", name: "Ελληνικά" }, // Greek (Modern)
+  { code: "kl", name: "Kalaallisut" }, // Greenlandic, Kalaallisut
+  { code: "gn", name: "Avañe'ẽ" }, // Guarani
+  { code: "gu", name: "ગુજરાતી" }, // Gujarati
+  { code: "ht", name: "Kreyòl ayisyen" }, // Haitian, Haitian Creole
+  { code: "ha", name: "Hausa" }, // Hausa
+  { code: "he", name: "עברית" }, // Hebrew
+  { code: "hz", name: "Otjiherero" }, // Herero
+  { code: "hi", name: "हिन्दी" }, // Hindi
+  { code: "ho", name: "Hiri Motu" }, // Hiri Motu
+  { code: "hu", name: "Magyar" }, // Hungarian
+  { code: "is", name: "Íslenska" }, // Icelandic
+  { code: "io", name: "Ido" }, // Ido
+  { code: "ig", name: "Igbo" }, // Igbo
+  { code: "id", name: "Bahasa Indonesia" }, // Indonesian
   {
     code: "ia",
-    name: "Interlingua (International Auxiliary Language Association)",
-  },
-  { code: "ie", name: "Interlingue" },
-  { code: "iu", name: "Inuktitut" },
-  { code: "ik", name: "Inupiaq" },
-  { code: "ga", name: "Irish" },
-  { code: "it", name: "Italian" },
-  { code: "ja", name: "Japanese" },
-  { code: "jv", name: "Javanese" },
-  { code: "kn", name: "Kannada" },
-  { code: "kr", name: "Kanuri" },
-  { code: "ks", name: "Kashmiri" },
-  { code: "kk", name: "Kazakh" },
-  { code: "rw", name: "Kinyarwanda" },
-  { code: "kv", name: "Komi" },
-  { code: "kg", name: "Kongo" },
-  { code: "ko", name: "Korean" },
-  { code: "kj", name: "Kwanyama, Kuanyama" },
-  { code: "ku", name: "Kurdish" },
-  { code: "ky", name: "Kyrgyz" },
-  { code: "lo", name: "Lao" },
-  { code: "la", name: "Latin" },
-  { code: "lv", name: "Latvian" },
-  { code: "lb", name: "Letzeburgesch, Luxembourgish" },
-  { code: "li", name: "Limburgish, Limburgan, Limburger" },
-  { code: "ln", name: "Lingala" },
-  { code: "lt", name: "Lithuanian" },
-  { code: "lu", name: "Luba-Katanga" },
-  { code: "mk", name: "Macedonian" },
-  { code: "mg", name: "Malagasy" },
-  { code: "ms", name: "Malay" },
-  { code: "ml", name: "Malayalam" },
-  { code: "mt", name: "Maltese" },
-  { code: "gv", name: "Manx" },
-  { code: "mi", name: "Maori" },
-  { code: "mr", name: "Marathi" },
-  { code: "mh", name: "Marshallese" },
-  { code: "ro", name: "Moldovan, Moldavian, Romanian" },
-  { code: "mn", name: "Mongolian" },
-  { code: "na", name: "Nauru" },
-  { code: "nv", name: "Navajo, Navaho" },
-  { code: "nd", name: "Northern Ndebele" },
-  { code: "ng", name: "Ndonga" },
-  { code: "ne", name: "Nepali" },
-  { code: "se", name: "Northern Sami" },
-  { code: "no", name: "Norwegian" },
-  { code: "nb", name: "Norwegian Bokmål" },
-  { code: "nn", name: "Norwegian Nynorsk" },
-  { code: "ii", name: "Nuosu, Sichuan Yi" },
-  { code: "oc", name: "Occitan (post 1500)" },
-  { code: "oj", name: "Ojibwa" },
-  { code: "or", name: "Oriya" },
-  { code: "om", name: "Oromo" },
-  { code: "os", name: "Ossetian, Ossetic" },
-  { code: "pi", name: "Pali" },
-  { code: "pa", name: "Panjabi, Punjabi" },
-  { code: "ps", name: "Pashto, Pushto" },
-  { code: "fa", name: "Persian" },
-  { code: "pl", name: "Polish" },
-  { code: "pt", name: "Portuguese" },
-  { code: "qu", name: "Quechua" },
-  { code: "rm", name: "Romansh" },
-  { code: "rn", name: "Rundi" },
-  { code: "ru", name: "Russian" },
-  { code: "sm", name: "Samoan" },
-  { code: "sg", name: "Sango" },
-  { code: "sa", name: "Sanskrit" },
-  { code: "sc", name: "Sardinian" },
-  { code: "sr", name: "Serbian" },
-  { code: "sn", name: "Shona" },
-  { code: "sd", name: "Sindhi" },
-  { code: "si", name: "Sinhala, Sinhalese" },
-  { code: "sk", name: "Slovak" },
-  { code: "sl", name: "Slovenian" },
-  { code: "so", name: "Somali" },
-  { code: "st", name: "Sotho, Southern" },
-  { code: "nr", name: "South Ndebele" },
-  { code: "es", name: "Spanish, Castilian" },
-  { code: "su", name: "Sundanese" },
-  { code: "sw", name: "Swahili" },
-  { code: "ss", name: "Swati" },
-  { code: "sv", name: "Swedish" },
-  { code: "tl", name: "Tagalog" },
-  { code: "ty", name: "Tahitian" },
-  { code: "tg", name: "Tajik" },
-  { code: "ta", name: "Tamil" },
-  { code: "tt", name: "Tatar" },
-  { code: "te", name: "Telugu" },
-  { code: "th", name: "Thai" },
-  { code: "bo", name: "Tibetan" },
-  { code: "ti", name: "Tigrinya" },
-  { code: "to", name: "Tonga (Tonga Islands)" },
-  { code: "ts", name: "Tsonga" },
-  { code: "tn", name: "Tswana" },
-  { code: "tr", name: "Turkish" },
-  { code: "tk", name: "Turkmen" },
-  { code: "tw", name: "Twi" },
-  { code: "ug", name: "Uighur, Uyghur" },
-  { code: "uk", name: "Ukrainian" },
-  { code: "ur", name: "Urdu" },
-  { code: "uz", name: "Uzbek" },
-  { code: "ve", name: "Venda" },
-  { code: "vi", name: "Vietnamese" },
-  { code: "vo", name: "Volap_k" },
-  { code: "wa", name: "Walloon" },
-  { code: "cy", name: "Welsh" },
-  { code: "fy", name: "Western Frisian" },
-  { code: "wo", name: "Wolof" },
-  { code: "xh", name: "Xhosa" },
-  { code: "yi", name: "Yiddish" },
-  { code: "yo", name: "Yoruba" },
-  { code: "za", name: "Zhuang, Chuang" },
-  { code: "zu", name: "Zulu" },
+    name: "Interlingua",
+  }, // Interlingua (International Auxiliary Language Association)
+  { code: "ie", name: "Interlingue" }, // Interlingue
+  { code: "iu", name: "ᐃᓄᒃᑎᑐᑦ" }, // Inuktitut
+  { code: "ik", name: "Iñupiaq" }, // Inupiaq
+  { code: "ga", name: "Gaeilge" }, // Irish
+  { code: "it", name: "Italiano" }, // Italian
+  { code: "ja", name: "日本語" }, // Japanese
+  { code: "jv", name: "Basa Jawa" }, // Javanese
+  { code: "kn", name: "ಕನ್ನಡ" }, // Kannada
+  { code: "kr", name: "Kanuri" }, // Kanuri
+  { code: "ks", name: "کٲشُر" }, // Kashmiri
+  { code: "kk", name: "Қазақ тілі" }, // Kazakh
+  { code: "rw", name: "Kinyarwanda" }, // Kinyarwanda
+  { code: "kv", name: "Коми" }, // Komi
+  { code: "kg", name: "Kikongo" }, // Kongo
+  { code: "ko", name: "한국어" }, // Korean
+  { code: "kj", name: "Kuanyama" }, // Kwanyama, Kuanyama
+  { code: "ku", name: "Kurdî" }, // Kurdish
+  { code: "ky", name: "Кыргызча" }, // Kyrgyz
+  { code: "lo", name: "ພາສາລາວ" }, // Lao
+  { code: "la", name: "Latine" }, // Latin
+  { code: "lv", name: "Latviešu" }, // Latvian
+  { code: "lb", name: "Lëtzebuergesch" }, // Letzeburgesch, Luxembourgish
+  { code: "li", name: "Limburgs" }, // Limburgish, Limburgan, Limburger
+  { code: "ln", name: "Lingála" }, // Lingala
+  { code: "lt", name: "Lietuvių" }, // Lithuanian
+  { code: "lu", name: "Kiluba" }, // Luba-Katanga
+  { code: "mk", name: "Македонски" }, // Macedonian
+  { code: "mg", name: "Malagasy" }, // Malagasy
+  { code: "ms", name: "Bahasa Melayu" }, // Malay
+  { code: "ml", name: "മലയാളം" }, // Malayalam
+  { code: "mt", name: "Malti" }, // Maltese
+  { code: "gv", name: "Gaelg" }, // Manx
+  { code: "mi", name: "Māori" }, // Maori
+  { code: "mr", name: "मराठी" }, // Marathi
+  { code: "mh", name: "Kajin M̧ajeļ" }, // Marshallese
+  { code: "ro", name: "Română" }, // Moldovan, Moldavian, Romanian
+  { code: "mn", name: "Монгол хэл" }, // Mongolian
+  { code: "na", name: "Ekaieidien" }, // Nauru
+  { code: "nv", name: "Diné bizaad" }, // Navajo, Navaho
+  { code: "nd", name: "isiNdebele" }, // Northern Ndebele
+  { code: "ng", name: "Oshindonga" }, // Ndonga
+  { code: "ne", name: "नेपाली" }, // Nepali
+  { code: "se", name: "Davvisámegiella" }, // Northern Sami
+  { code: "no", name: "Norsk" }, // Norwegian
+  { code: "nb", name: "Norsk bokmål" }, // Norwegian Bokmål
+  { code: "nn", name: "Norsk nynorsk" }, // Norwegian Nynorsk
+  { code: "ii", name: "ꆈꌠ꒿ Nuosuhxop" }, // Nuosu, Sichuan Yi
+  { code: "oc", name: "Occitan" }, // Occitan (post 1500)
+  { code: "oj", name: "Ojibwemowin" }, // Ojibwa
+  { code: "or", name: "ଓଡ଼ିଆ" }, // Oriya
+  { code: "om", name: "Afaan Oromoo" }, // Oromo
+  { code: "os", name: "Ирон æвзаг" }, // Ossetian, Ossetic
+  { code: "pi", name: "पाळि" }, // Pali
+  { code: "pa", name: "ਪੰਜਾਬੀ" }, // Panjabi, Punjabi
+  { code: "ps", name: "پښتو" }, // Pashto, Pushto
+  { code: "fa", name: "فارسی" }, // Persian
+  { code: "pl", name: "Polski" }, // Polish
+  { code: "pt", name: "Português" }, // Portuguese
+  { code: "qu", name: "Runa simi" }, // Quechua
+  { code: "rm", name: "Rumantsch" }, // Romansh
+  { code: "rn", name: "Kirundi" }, // Rundi
+  { code: "ru", name: "Русский" }, // Russian
+  { code: "sm", name: "Gagana Sāmoa" }, // Samoan
+  { code: "sg", name: "Sängö" }, // Sango
+  { code: "sa", name: "संस्कृतम्" }, // Sanskrit
+  { code: "sc", name: "Sardu" }, // Sardinian
+  { code: "sr", name: "Српски" }, // Serbian
+  { code: "sn", name: "chiShona" }, // Shona
+  { code: "sd", name: "سنڌي" }, // Sindhi
+  { code: "si", name: "සිංහල" }, // Sinhala, Sinhalese
+  { code: "sk", name: "Slovenčina" }, // Slovak
+  { code: "sl", name: "Slovenščina" }, // Slovenian
+  { code: "so", name: "Soomaali" }, // Somali
+  { code: "st", name: "Sesotho" }, // Sotho, Southern
+  { code: "es", name: "Español" }, // Spanish
+  { code: "su", name: "Basa Sunda" }, // Sundanese
+  { code: "sw", name: "Kiswahili" }, // Swahili
+  { code: "ss", name: "SiSwati" }, // Swati
+  { code: "sv", name: "Svenska" }, // Swedish
+  { code: "tl", name: "Tagalog" }, // Tagalog
+  { code: "ty", name: "Reo Tahiti" }, // Tahitian
+  { code: "tg", name: "Тоҷикӣ" }, // Tajik
+  { code: "ta", name: "தமிழ்" }, // Tamil
+  { code: "tt", name: "Татарча" }, // Tatar
+  { code: "te", name: "తెలుగు" }, // Telugu
+  { code: "th", name: "ไทย" }, // Thai
+  { code: "bo", name: "བོད་སྐད་" }, // Tibetan
+  { code: "ti", name: "ትግርኛ" }, // Tigrinya
+  { code: "to", name: "Faka-Tonga" }, // Tonga (Tonga Islands)
+  { code: "ts", name: "Xitsonga" }, // Tsonga
+  { code: "tn", name: "Setswana" }, // Tswana
+  { code: "tr", name: "Türkçe" }, // Turkish
+  { code: "tk", name: "Türkmen dili" }, // Turkmen
+  { code: "tw", name: "Twi" }, // Twi
+  { code: "ug", name: "ئۇيغۇرچە" }, // Uighur, Uyghur
+  { code: "uk", name: "Українська" }, // Ukrainian
+  { code: "ur", name: "اردو" }, // Urdu
+  { code: "ve", name: "Tshivenḓa" }, // Venda
+  { code: "vi", name: "Tiếng Việt" }, // Vietnamese
+  { code: "vo", name: "Volapük" }, // Volapük
+  { code: "wa", name: "Walon" }, // Walloon
+  { code: "cy", name: "Cymraeg" }, // Welsh
+  { code: "fy", name: "Frysk" }, // Western Frisian
+  { code: "wo", name: "Wolof" }, // Wolof
+  { code: "xh", name: "isiXhosa" }, // Xhosa
+  { code: "yi", name: "ייִדיש" }, // Yiddish
+  { code: "yo", name: "Yorùbá" }, // Yoruba
+  { code: "za", name: "Cuengh" }, // Zhuang, Chuang
+  { code: "zu", name: "isiZulu" }, // Zulu
 ] as const;
-
 
 export type LanguageCode = (typeof languages)[number]["code"];
 export type LanguageName = (typeof languages)[number]["name"];
+export type LanguageOption = Record<LanguageCode, LanguageName>;
 
 export const LANGUAGE_LIST: readonly {
   code: LanguageCode;
   name: LanguageName;
 }[] = languages;
-export const LANGUAGE_NAME_MAP: Record<LanguageCode, LanguageName> = languages.reduce(
-  (acc, lang) => {
+export const LANGUAGE_NAME_MAP: LanguageOption =
+  languages.reduce((acc, lang) => {
     acc[lang.code] = lang.name;
     return acc;
-  },
-  {} as Record<LanguageCode, LanguageName>
-);
-export const LANGUAGE_CODE_MAP: Record<LanguageName, LanguageCode> = languages.reduce(
-  (acc, lang) => {
+  }, {} as LanguageOption);
+export const LANGUAGE_CODE_MAP: LanguageOption =
+  languages.reduce((acc, lang) => {
     acc[lang.name] = lang.code;
     return acc;
-  },
-  {} as Record<LanguageName, LanguageCode>
-);
+  }, {} as LanguageOption);
