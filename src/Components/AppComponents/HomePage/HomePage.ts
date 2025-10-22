21df0eb59a5ab8130c66a97c78ecc8421eac0e6b
Tomas Santana <tomas.30604530@uru.edu>
2025-10-21 20:13:44 -0400

    feat: initialize IndexedDBService in HomePage component

--- src/Components/AppComponents/HomePage/HomePage.ts
+++ src/Components/AppComponents/HomePage/HomePage.ts
@@ -1,16 +1,19 @@
-import build from "../../../lib/build";
+import { openDatabase } from "../../Service/DB/openDatabase";
+import { IndexedDBService } from "c:/Users/tommy/Documents/URU/componentes/flashcards/src/Components/Service/DB/IndexedDB";
 
 export default class HomePage extends HTMLElement {
+  db: IndexedDBService;
   constructor(props: any) {
     super();
     window.slice.attachTemplate(this);
 
     window.slice.controller.setComponentProps(this, props);
-    window.slice.setTheme("light");
+    this.db = openDatabase();
   }
 
   async init() {
   }
+
 }
 
 customElements.define("slice-home-page", HomePage);
