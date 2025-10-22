4a9c904bbe90e65b447ab687ff2b60a0abe11a8a
Tomas Santana <tomas.30604530@uru.edu>
2025-10-22 17:54:44 -0400

    feat: add SidebarLink and SidebarToggle types to component definitions

--- src/components.gen.ts
+++ src/components.gen.ts
@@ -6,6 +6,8 @@ import type { LogoProps } from './Components/Visual/Logo/Logo.types';
 import type { MultiRouteProps } from './Components/Visual/MultiRoute/MultiRoute.types';
 import type { SelectProps } from './Components/Visual/Select/Select.types';
 import type { SidebarProps } from './Components/Visual/Sidebar/Sidebar.types';
+import type { SidebarLinkProps } from './Components/Visual/SidebarLink/SidebarLink.types';
+import type { SidebarToggleProps } from './Components/Visual/SidebarToggle/SidebarToggle.types';
 import type { StartPageProps } from './Components/AppComponents/StartPage/StartPage.types';
 import type Button from './Components/Visual/Button/Button';
 import type Input from './Components/Visual/Input/Input';
@@ -13,11 +15,12 @@ import type Logo from './Components/Visual/Logo/Logo';
 import type MultiRoute from './Components/Visual/MultiRoute/MultiRoute';
 import type Select from './Components/Visual/Select/Select';
 import type Sidebar from './Components/Visual/Sidebar/Sidebar';
-import type EventManager from './Components/Service/EventManager/EventManager';
+import type SidebarLink from './Components/Visual/SidebarLink/SidebarLink';
+import type SidebarToggle from './Components/Visual/SidebarToggle/SidebarToggle';
 import type HomePage from './Components/AppComponents/HomePage/HomePage';
 import type StartPage from './Components/AppComponents/StartPage/StartPage';
 
-export type ComponentName = 'Button' | 'Card' | 'Checkbox' | 'CodeVisualizer' | 'Details' | 'DropDown' | 'Grid' | 'Icon' | 'Input' | 'Layout' | 'Loading' | 'Logo' | 'MultiRoute' | 'Navbar' | 'NotFound' | 'Route' | 'Select' | 'Sidebar' | 'Switch' | 'TreeItem' | 'TreeView' | 'EventManager' | 'FetchManager' | 'Link' | 'LocalStorageManager' | 'Translator' | 'HomePage' | 'Playground' | 'StartPage';
+export type ComponentName = 'Button' | 'Card' | 'Checkbox' | 'CodeVisualizer' | 'Details' | 'DropDown' | 'Grid' | 'Icon' | 'Input' | 'Layout' | 'Loading' | 'Logo' | 'MultiRoute' | 'Navbar' | 'NotFound' | 'Route' | 'Select' | 'Sidebar' | 'SidebarLink' | 'SidebarToggle' | 'Switch' | 'TreeItem' | 'TreeView' | 'EventManager' | 'FetchManager' | 'Link' | 'LocalStorageManager' | 'Translator' | 'HomePage' | 'Playground' | 'StartPage';
 
 export type ComponentPropsMap = {
   'Button': ButtonProps,
@@ -38,6 +41,8 @@ export type ComponentPropsMap = {
   'Route': any,
   'Select': SelectProps,
   'Sidebar': SidebarProps,
+  'SidebarLink': SidebarLinkProps,
+  'SidebarToggle': SidebarToggleProps,
   'Switch': any,
   'TreeItem': any,
   'TreeView': any,
@@ -69,6 +74,8 @@ export type ComponentInstanceMap = {
   'Route': any,
   'Select': Select,
   'Sidebar': Sidebar,
+  'SidebarLink': SidebarLink,
+  'SidebarToggle': SidebarToggle,
   'Switch': any,
   'TreeItem': any,
   'TreeView': any,
