5c9eb567339872239038b45cd808756311b70280
Tomas Santana <tomas.30604530@uru.edu>
2025-10-27 22:37:35 -0400

    feat: add types and components for DeckCard, Dialog, EditCardModal, Flashcard, NewCardModal, PageTitle, SButton, Selectable, SIcon, StartPracticeModal, FlashcardsPage, and PracticePage

--- src/components.gen.ts
+++ src/components.gen.ts
@@ -1,34 +1,62 @@
 // AUTO-GENERATED FILE. Do not edit manually.
 // Updated by src/scripts/createComponent.js
 import type { ButtonProps } from './Components/Visual/Button/Button.types';
+import type { DeckCardProps } from './Components/Visual/DeckCard/DeckCard.types';
+import type { DialogProps } from './Components/Visual/Dialog/Dialog.types';
+import type { EditCardModalProps } from './Components/Visual/EditCardModal/EditCardModal.types';
+import type { FlashcardProps } from './Components/Visual/Flashcard/Flashcard.types';
 import type { InputProps } from './Components/Visual/Input/Input.types';
 import type { LogoProps } from './Components/Visual/Logo/Logo.types';
 import type { MultiRouteProps } from './Components/Visual/MultiRoute/MultiRoute.types';
+import type { NewCardModalProps } from './Components/Visual/NewCardModal/NewCardModal.types';
+import type { PageTitleProps } from './Components/Visual/PageTitle/PageTitle.types';
+import type { SButtonProps } from './Components/Visual/SButton/SButton.types';
 import type { SelectProps } from './Components/Visual/Select/Select.types';
+import type { SelectableProps } from './Components/Visual/Selectable/Selectable.types';
+import type { SIconProps } from './Components/Visual/SIcon/SIcon.types';
 import type { SidebarProps } from './Components/Visual/Sidebar/Sidebar.types';
 import type { SidebarLinkProps } from './Components/Visual/SidebarLink/SidebarLink.types';
 import type { SidebarToggleProps } from './Components/Visual/SidebarToggle/SidebarToggle.types';
+import type { StartPracticeModalProps } from './Components/Visual/StartPracticeModal/StartPracticeModal.types';
+import type { FlashcardsPageProps } from './Components/AppComponents/FlashcardsPage/FlashcardsPage.types';
+import type { PracticePageProps } from './Components/AppComponents/PracticePage/PracticePage.types';
 import type { StartPageProps } from './Components/AppComponents/StartPage/StartPage.types';
 import type Button from './Components/Visual/Button/Button';
+import type DeckCard from './Components/Visual/DeckCard/DeckCard';
+import type Dialog from './Components/Visual/Dialog/Dialog';
+import type EditCardModal from './Components/Visual/EditCardModal/EditCardModal';
+import type Flashcard from './Components/Visual/Flashcard/Flashcard';
 import type Input from './Components/Visual/Input/Input';
 import type Logo from './Components/Visual/Logo/Logo';
 import type MultiRoute from './Components/Visual/MultiRoute/MultiRoute';
+import type NewCardModal from './Components/Visual/NewCardModal/NewCardModal';
+import type PageTitle from './Components/Visual/PageTitle/PageTitle';
+import type SButton from './Components/Visual/SButton/SButton';
 import type Select from './Components/Visual/Select/Select';
+import type Selectable from './Components/Visual/Selectable/Selectable';
+import type SIcon from './Components/Visual/SIcon/SIcon';
 import type Sidebar from './Components/Visual/Sidebar/Sidebar';
 import type SidebarLink from './Components/Visual/SidebarLink/SidebarLink';
 import type SidebarToggle from './Components/Visual/SidebarToggle/SidebarToggle';
+import type StartPracticeModal from './Components/Visual/StartPracticeModal/StartPracticeModal';
+import type FlashcardsPage from './Components/AppComponents/FlashcardsPage/FlashcardsPage';
 import type HomePage from './Components/AppComponents/HomePage/HomePage';
+import type PracticePage from './Components/AppComponents/PracticePage/PracticePage';
 import type StartPage from './Components/AppComponents/StartPage/StartPage';
 
-export type ComponentName = 'Button' | 'Card' | 'Checkbox' | 'CodeVisualizer' | 'Details' | 'DropDown' | 'Grid' | 'Icon' | 'Input' | 'Layout' | 'Loading' | 'Logo' | 'MultiRoute' | 'Navbar' | 'NotFound' | 'Route' | 'Select' | 'Sidebar' | 'SidebarLink' | 'SidebarToggle' | 'Switch' | 'TreeItem' | 'TreeView' | 'EventManager' | 'FetchManager' | 'Link' | 'LocalStorageManager' | 'Translator' | 'HomePage' | 'Playground' | 'StartPage';
+export type ComponentName = 'Button' | 'Card' | 'Checkbox' | 'CodeVisualizer' | 'DeckCard' | 'Details' | 'Dialog' | 'DropDown' | 'EditCardModal' | 'Flashcard' | 'Grid' | 'Icon' | 'Input' | 'Layout' | 'Loading' | 'Logo' | 'MultiRoute' | 'Navbar' | 'NewCardModal' | 'NotFound' | 'PageTitle' | 'Route' | 'SButton' | 'Select' | 'Selectable' | 'SIcon' | 'Sidebar' | 'SidebarLink' | 'SidebarToggle' | 'StartPracticeModal' | 'Switch' | 'TreeItem' | 'TreeView' | 'EventManager' | 'FetchManager' | 'Link' | 'LocalStorageManager' | 'Translator' | 'FlashcardsPage' | 'HomePage' | 'Playground' | 'PracticePage' | 'StartPage';
 
 export type ComponentPropsMap = {
   'Button': ButtonProps,
   'Card': any,
   'Checkbox': any,
   'CodeVisualizer': any,
+  'DeckCard': DeckCardProps,
   'Details': any,
+  'Dialog': DialogProps,
   'DropDown': any,
+  'EditCardModal': EditCardModalProps,
+  'Flashcard': FlashcardProps,
   'Grid': any,
   'Icon': any,
   'Input': InputProps,
@@ -37,12 +65,18 @@ export type ComponentPropsMap = {
   'Logo': LogoProps,
   'MultiRoute': MultiRouteProps,
   'Navbar': any,
+  'NewCardModal': NewCardModalProps,
   'NotFound': any,
+  'PageTitle': PageTitleProps,
   'Route': any,
+  'SButton': SButtonProps,
   'Select': SelectProps,
+  'Selectable': SelectableProps,
+  'SIcon': SIconProps,
   'Sidebar': SidebarProps,
   'SidebarLink': SidebarLinkProps,
   'SidebarToggle': SidebarToggleProps,
+  'StartPracticeModal': StartPracticeModalProps,
   'Switch': any,
   'TreeItem': any,
   'TreeView': any,
@@ -50,8 +84,10 @@ export type ComponentPropsMap = {
   'Link': any,
   'LocalStorageManager': any,
   'Translator': any,
+  'FlashcardsPage': FlashcardsPageProps,
   'HomePage': any,
   'Playground': any,
+  'PracticePage': PracticePageProps,
   'StartPage': StartPageProps
 };
 
@@ -60,8 +96,12 @@ export type ComponentInstanceMap = {
   'Card': any,
   'Checkbox': any,
   'CodeVisualizer': any,
+  'DeckCard': DeckCard,
   'Details': any,
+  'Dialog': Dialog,
   'DropDown': any,
+  'EditCardModal': EditCardModal,
+  'Flashcard': Flashcard,
   'Grid': any,
   'Icon': any,
   'Input': Input,
@@ -70,12 +110,18 @@ export type ComponentInstanceMap = {
   'Logo': Logo,
   'MultiRoute': MultiRoute,
   'Navbar': any,
+  'NewCardModal': NewCardModal,
   'NotFound': any,
+  'PageTitle': PageTitle,
   'Route': any,
+  'SButton': SButton,
   'Select': Select,
+  'Selectable': Selectable,
+  'SIcon': SIcon,
   'Sidebar': Sidebar,
   'SidebarLink': SidebarLink,
   'SidebarToggle': SidebarToggle,
+  'StartPracticeModal': StartPracticeModal,
   'Switch': any,
   'TreeItem': any,
   'TreeView': any,
@@ -83,7 +129,9 @@ export type ComponentInstanceMap = {
   'Link': any,
   'LocalStorageManager': any,
   'Translator': any,
+  'FlashcardsPage': FlashcardsPage,
   'HomePage': HomePage,
   'Playground': any,
+  'PracticePage': PracticePage,
   'StartPage': StartPage
 };
