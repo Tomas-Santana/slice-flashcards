8716006a03b50e8de1a7bd8d5b088489c0603a9c
Tomas Santana <tomas.30604530@uru.edu>
2025-10-27 22:36:50 -0400

    feat: add LanguageRecord type for language code mapping

--- src/Components/Service/DB/models/common.ts
+++ src/Components/Service/DB/models/common.ts
@@ -1,3 +1,4 @@
+import { LanguageCode } from "@/lib/types/languages";
 
 
 export type DifficultyBand = "basic" | "intermediate" | "advanced";
@@ -5,4 +6,6 @@ export type DifficultyBand = "basic" | "intermediate" | "advanced";
 export type PersonalDifficulty = 1 | 2 | 3 | 4 | 5;
 
 export type StudyMode = "free" | "training";
+
+export type LanguageRecord = Record<LanguageCode, string>; 
 
