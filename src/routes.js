75609e0995333817d137e31f32e0ee0eeedeb157
Tomas Santana <tomas.30604530@uru.edu>
2025-10-30 05:29:31 -0400

    feat: add dynamic deckId parameter to practice route for improved navigation

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 const routes = [
    // Rutas principales
    { path: '/', component: 'HomePage', metadata: {} },
+   { path: '/practice/${deckId}', component: 'Tester', metadata: {} },
    { path: '/start', component: 'StartPage', metadata: { requiresNoSettings: true} },
    { path: '/404', component: 'NotFound', metadata: {} },
 
